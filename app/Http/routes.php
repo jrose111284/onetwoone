e6fa066defac81c397b4194b4dabffe5fce634c3
jamie rose <jrose111284@gmail.com>
2016-11-11 16:54:03 +0000

    can now to full crud with a has one database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 // creates a address inside the address table with the same id as the user from the user table
 Route::get('/insert', function () {
 
@@ -24,9 +25,31 @@ Route::get('/insert', function () {
     $user = User::findOrFail(1);
 
     //saving the address as a var
-    $address = new Address(['address_name'=> '123 southampton road']);
+    $address = new Address(['address_name'=> '12345 southampton road']);
 
     //putting the address function and saving the address that was created
     $user->address()->save($address);
 
+});
+//updates the address table
+Route::get('/update', function () {
+
+    //updates the database of address were the user id is 1, so if you have more then one will update them all
+    $address = Address::where('user_id', 1 )->first(); // first will apply to the fir
+    $address->address_name = '123 my road';
+
+    $address->save();
+});
+git add .
+//reads the database from the user with the same id as the address id
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+
+   echo $user->address->created_at;
+});
+
+Route::get('/delete', function(){
+   $user = User::findOrFail(1);
+
+    $user->address()->delete();
 });
